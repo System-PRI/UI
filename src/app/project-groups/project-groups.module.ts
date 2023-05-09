f229f6678c370236573ff74188675796b6e22cc7
Adrian Kuraszkiewicz <goieh@WPLT002513.bayer.cnb>
2023-05-09 23:16:19 +0200

    add title, project button, space for instructors list component and RWD styles

--- src/app/project-groups/project-groups.module.ts
+++ src/app/project-groups/project-groups.module.ts
@@ -5,6 +5,7 @@ import { ProjectGroupsRoutingModule } from './project-groups-routing.module';
 import { ProjectGroupsListComponent } from './project-groups-list/project-groups-list.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import {MatPaginatorModule} from '@angular/material/paginator';
     CommonModule,
     ProjectGroupsRoutingModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule
   ]
 })
 export class ProjectGroupsModule { }
