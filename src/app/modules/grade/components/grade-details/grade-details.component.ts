d29ca5d6d71c9798734598d61e87bc9b8e4ca335
AdrianNewPC <akuratweb@gmail.com>
2023-11-01 17:49:29 +0100

    creating form

--- src/app/modules/grade/components/grade-details/grade-details.component.ts
+++ src/app/modules/grade/components/grade-details/grade-details.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { GradeDetails } from '../../models/grade';
 import { Subject } from 'rxjs';
 import { UserState } from 'src/app/modules/user/state/user.state';
-import { AbstractControl, FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'grade-details',
@@ -15,32 +15,7 @@ export class GradeDetailsComponent implements OnInit, OnDestroy {
   data!: GradeDetails;
   unsubscribe$ = new Subject();
   user!: UserState;
-  gradesData = [
-    {
-      section: '1',
-      group: '1',
-      grade: 1
-    },
-    {
-      section: '1',
-      group: '2',
-      grade: 1
-    }
-  ]
-  gradeForm = this.fb.group({
-    grades: this.fb.array([
-      {
-        section: '1',
-        group: '1',
-        grade: new FormControl(1)
-      },
-      {
-        section: '1',
-        group: '2',
-        grade: new FormControl(2)
-      }
-    ]),
-  });
+  gradeForm = this.fb.group<{[key: string]: FormGroup }>({});
 
   constructor(private activatedRoute: ActivatedRoute, private router: Router, private fb: FormBuilder) { }
 
@@ -48,6 +23,14 @@ export class GradeDetailsComponent implements OnInit, OnDestroy {
     this.activatedRoute.data.subscribe(({gradeDetails, user}) => {
       this.data = gradeDetails;
       this.user = user;
+
+      this.data.sections.forEach(section => {
+        this.gradeForm.addControl(section.id, this.fb.group<{[key: string]: FormControl }>({}))
+
+        section.criteriaGroups.forEach(group => {
+          this.gradeForm.controls[section.id].addControl(group.id, new FormControl(group.selectedCriterion))
+        })
+      })
     })
   }
 
@@ -55,9 +38,9 @@ export class GradeDetailsComponent implements OnInit, OnDestroy {
     this.router.navigate([{outlets: {modal: null}}]);
   }
 
+
   isRadioButtonChecked(sectionIndex: string, groupIndex: string, value: number): boolean {
-    //console.log((this.grades.controls.filter((grade: any) => grade.section === sectionIndex && grade.group === groupIndex)[0] as FormControl).value.grade as FormControl);
-    return (this.grades.controls.filter((grade: AbstractControl) => grade.value.section === sectionIndex && grade.value.group === groupIndex)[0].value.grade as FormControl).value === value;
+    return this.gradeForm.controls[sectionIndex].controls[groupIndex].value === value
   }
 
 
@@ -66,13 +49,14 @@ export class GradeDetailsComponent implements OnInit, OnDestroy {
     this.unsubscribe$.complete()
   }
 
-  getGradeData(sectionIndex: string, groupIndex: string): FormControl {
-    return this.grades.controls.filter((grade: AbstractControl) => grade.value.section === sectionIndex && grade.value.group === groupIndex)[0].value.grade as FormControl;
-  }
   
+  onSubmit(): void {
+    if (this.gradeForm.valid) {    
+      console.log(this.gradeForm.value)
+    }
+  }
 
-  get grades() {
-    return this.gradeForm.get('grades') as FormArray;
+  getGradeData(sectionIndex: string, groupIndex: string): FormControl {
+    return this.gradeForm.controls[sectionIndex].controls[groupIndex] as FormControl
   }
-  
 }
